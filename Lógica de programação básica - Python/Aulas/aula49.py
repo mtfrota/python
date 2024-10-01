cac181e775979a5478db0572d3374fe7606ae403
mtfrota <frota.amteus.16@gmail.com>
2024-09-30 22:29:28 -0300

    atividades e aulas

---
+++ Lógica de programação básica - Python/Aulas/aula49.py
@@ -0,0 +1,20 @@
+# Listas em python
+# Tipo list - Mutável
+# Suprta vários valores de qualquer tipo
+# Conhecimento reutilizaveis - indices e fatiamento
+# Métodos úteis:
+#     append - adiciona um item ao finall
+#     insert - adiciona um item no índice escolhido
+#     pop - remove do final ou do índice escolhido
+#     del - apaga um índice
+#     clear - limpa a lista
+#     extend - estende a lista
+#     + - concatena listas
+# Creat Read Update Delete
+# Cirear ler alterar apagar = list[i] (crud)
+
+list_a = []
+list_b = []
+list_c = list_a + list_b
+list_d = 
+print(list_c)
